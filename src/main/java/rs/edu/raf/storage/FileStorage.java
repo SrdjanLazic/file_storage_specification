cbef6745a210447a9bdf514a98343b53d7812c0c
SrdjanLazic <slazic3219rn@raf.rs>
2021-11-05 13:02:05 +0100

    Dodate nove metode za konfiguraciju skladista

--- src/main/java/rs/edu/raf/storage/FileStorage.java
+++ src/main/java/rs/edu/raf/storage/FileStorage.java
@@ -5,22 +5,21 @@ public interface FileStorage {
     // Operacije nad skladistem:
     // ----------------------------------------------------------
 
-    // ako unapred zadajemo putanju :
-    void createFolder(String path, String folderName);
+    void createFolder(String path, String folderName); // ako unapred zadajemo putanju
     void createFile(String path, String filename);
-    // ako smestamo u korenski dir:
-    void createFolder(String folderName);
+    void createFolder(String folderName); // ako smestamo u korenski dir
     void createFile(String filename);
     void delete(String path);
     void move(String source, String destination);
     void list(); // default implementacija bez argumenata: prikazi sve foldere i fajlove u root-u skladista
     void list(String argument, Operations operation);
     // TODO: put metoda (smestanje):
+
     void get(String path);  // preuzimanje u local storage-u je smestanje u neki ranije definisani folder
 
 
 
-    // Konfiguracija skladsita:
+    // TODO: Konfiguracija skladsita (work in progress):
     // ------------------------------------------------------------
 
     void initializeStorage(String path);
