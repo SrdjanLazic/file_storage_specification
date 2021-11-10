9579a8ee389a77f84c4bd8b096a1a702b3e79ca5
SrdjanLazic <slazic3219rn@raf.rs>
2021-11-10 17:56:19 +0100

    Refaktorisanje klasa, koda

--- src/main/java/rs/edu/raf/storage/FileStorage.java
+++ src/main/java/rs/edu/raf/storage/FileStorage.java
@@ -1,7 +1,12 @@
 package rs.edu.raf.storage;
 
 import rs.edu.raf.storage.enums.Operations;
+import rs.edu.raf.storage.enums.Privileges;
+import rs.edu.raf.storage.exceptions.FileNotFoundException;
+import rs.edu.raf.storage.exceptions.InsufficientPrivilegesException;
 import rs.edu.raf.storage.exceptions.InvalidExtensionException;
+
+import java.util.Set;
 
 public interface FileStorage {
 
@@ -12,13 +17,23 @@ public interface FileStorage {
     void createFile(String path, String filename) throws InvalidExtensionException;
     void createFolder(String folderName); // ako smestamo u korenski dir
     void createFile(String filename) throws InvalidExtensionException;
-    void delete(String path);
+    void delete(String path) throws FileNotFoundException, InsufficientPrivilegesException;
     void move(String source, String destination);
     void put(String source, String destination);
     void list(); // default implementacija bez argumenata: prikazi sve foldere i fajlove u root-u skladista
     void list(String argument, Operations operation);
 
     void get(String path);  // preuzimanje u local storage-u je smestanje u neki ranije definisani folder
+
+    // TODO: work in progress...
+    void initializeStorage(String path) throws InsufficientPrivilegesException;
+    void limitNumberOfFiles(int number, String directory) throws InsufficientPrivilegesException;
+    void limitStorageSize(long sizeInBytes) throws InsufficientPrivilegesException;
+    void restrictExtension(String extension) throws InsufficientPrivilegesException;
+
+    // TODO: work in progress...
+    void addNewUser(AbstractUser user, Set<Privileges> privilegesSet);
+    void disconnectUser(AbstractUser user);
 
 
 
