21e2a62e0fb311a40aee03c4592fb36a9759feb7
Reljadin <rivanovic10120rn@raf.rs>
2021-11-16 19:50:42 +0100

    dokumentacija gotova - javadoc TODO

--- src/main/java/rs/edu/raf/storage/enums/Operations.java
+++ src/main/java/rs/edu/raf/storage/enums/Operations.java
@@ -1,5 +1,12 @@
 package rs.edu.raf.storage.enums;
 
+/**
+ * Operacije koje predstavljaju mogucnost pretrage fajlova prilikom listanja
+ * FILTER_EXTENSION za ciljanu ekstenziju
+ * FILTER_FILENAME za pronalazenje upisane reci medju imenima fajlova
+ * FILTER_BY_NAME pretraga po imenu po uzlaznimm ili silaznim slovima sortirano
+ * FILTER_BY_DATE pretraga po datumu ili kreiranja ili modifikovanja fajlova sortirano
+ */
 public enum Operations {
     FILTER_EXTENSION, FILTER_FILENAME, SORT_BY_NAME_ASC, SORT_BY_NAME_DESC, SORT_BY_DATE_CREATED, SORT_BY_DATE_MODIFIED_ASC, SORT_BY_DATE_MODIFIED_DESC
 }
--- src/main/java/rs/edu/raf/storage/enums/Privileges.java
+++ src/main/java/rs/edu/raf/storage/enums/Privileges.java
@@ -1,5 +1,13 @@
 package rs.edu.raf.storage.enums;
 
+/**
+ * Privilegije koje korisnici mogu imati (od kojih superuser poseduje sve)
+ * VIEW - pregled svih fajlova sa skladista (i mogucnost listanja istih)
+ * DOWNLOAD - mogucnost preuzimanja fajlova sa skladista
+ * CREATE - mogucnost snimanja, kreiranja i pomeranja fajlova unutar skladista
+ * DELETE - mogucnost brisanja fajlova sa skladista
+ * Jedino superuser moze imati privilegije za postavljanje konfiguracije skladista i korisnicke administracije
+ */
 public enum Privileges {
     VIEW, CREATE, DELETE, DOWNLOAD
 }
